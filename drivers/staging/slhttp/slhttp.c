e75ccc83453c0a4ccf789b29ec1b13ac06d76c91
Willy Tarreau <w@1wt.eu>
2013-12-07 21:25:26 +0100

    staging: slhttp: return a SYN-ACK in response to a SYN
    We now detect a SYN and respond with a reversed SYN/ACK.
    The ISN choosen for the SYN ACK is shifted 4 bits left so that
    we can use the lower 4 bits to store a state.
    
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- drivers/staging/slhttp/slhttp.c
+++ drivers/staging/slhttp/slhttp.c
@@ -16,7 +16,9 @@
 #include <net/rtnetlink.h>
 #include <linux/u64_stats_sync.h>
 #include <net/sock.h>
+#include <net/tcp.h>
 #include <linux/ip.h>
+#include <linux/tcp.h>
 
 struct pcpu_dstats {
 	u64			tx_packets;
@@ -26,6 +28,21 @@ struct pcpu_dstats {
 	u64			rx_bytes;
 	struct u64_stats_sync	syncp;
 };
+
+enum {
+	SLH_ST_REQ = 0,
+	SLH_ST_LASTACK = 1,
+	SLH_ST_ACK_CL_LAST = 2,
+	SLH_ST_ACK_CL_FIN = 3,
+	/* other states are not as much important */
+};
+
+/* This one has to be increased by 16 for each SYN emitted. It does not
+ * require any locking as we only increase it to avoid confuse the client
+ * in case we get a late packet.
+ * We can have an array of a few isns per source port hash if needed.
+ */
+static u32 isn;
 
 static struct rtnl_link_stats64 *slhttp_get_stats64(struct net_device *dev,
 						   struct rtnl_link_stats64 *stats)
@@ -56,58 +73,159 @@ static struct rtnl_link_stats64 *slhttp_get_stats64(struct net_device *dev,
 	return stats;
 }
 
-static netdev_tx_t slhttp_xmit(struct sk_buff *skb, struct net_device *dev)
+static void slhttp_reply_to_skb(struct net_device *dev, struct sk_buff *skb, int *op, int *ob)
 {
-	struct pcpu_dstats *dstats = this_cpu_ptr(dev->dstats);
-	int len, ihl;
 	struct iphdr *ih;
-	u32 daddr;
-	u32 saddr;
+	struct tcphdr *th;
+	u32 ack, st, seq;
+	u32 dst, src;
+	u16 spt, dpt, id;
+	u32 ihl;
+	u32 thlen;
+	struct sk_buff *pkt1, *pkt2;
+
+	//printk("@%d: skb->head=%p, data=%d, tail=%d, end=%d, len=%d\n", __LINE__,
+	//       skb->head,
+	//       (int)(skb->data - skb->head),
+	//       (int)(skb_tail_pointer(skb) - skb->head),
+	//       (int)(skb_end_pointer(skb) - skb->head),
+	//       (int)skb->len);
 
 	skb->protocol = eth_type_trans(skb, dev);
-
-	len = skb->len;
-
 	if (skb->protocol != htons(ETH_P_IP))
-		goto drop;
+		return;
+
+	/* we're on a no-arp device, recover the ethernet header */
+	skb_push(skb, 14);
 
 	if (unlikely(!pskb_may_pull(skb, sizeof(*ih))))
-		goto drop;
+		return;
 
 	ih = ip_hdr(skb);
 	ihl = ih->ihl * 4;
 	if (ihl < sizeof(*ih))
-		goto drop;
+		return;
 
-	skb_orphan(skb);
+	if (ih->protocol != 6)
+		return;
 
-	saddr = ih->saddr;
-	daddr = ih->daddr;
+	th = tcp_hdr(skb);
 
-	ih->saddr = daddr;
-	ih->daddr = saddr;
+	thlen = th->doff * 4;
+	if (thlen < sizeof(*th))
+		return;
 
-	/* Before queueing this packet to netif_rx(),
-	 * make sure dst is refcounted.
-	 */
-	skb_dst_force(skb);
+	if (!pskb_may_pull(skb, thlen))
+		return;
 
-	local_bh_disable();
-	netif_receive_skb(skb);
-	local_bh_enable();
+	if (th->syn) {
+		/* build a SYN-ACK inside the packet existing skb */
+		pkt1 = netdev_alloc_skb_ip_align(dev, 14 + 20 + 24);
+
+		//pkt1 = netdev_alloc_skb(dev, 14 + 20 + 24);
+		//if (!pkt1)
+		//	return;
+		//skb_reserve(pkt1, 2);
+
+		//printk("@%d: pkt1->head=%p, data=%d, tail=%d, end=%d, len=%d\n", __LINE__,
+		//       pkt1->head,
+		//       (int)(pkt1->data - pkt1->head),
+		//       (int)(skb_tail_pointer(pkt1) - pkt1->head),
+		//       (int)(skb_end_pointer(pkt1) - pkt1->head),
+		//       (int)pkt1->len);
+
+
+		/* build ethernet : in theory we should swap src/dst and keep
+		 * the protocol, but in practice we're on a noarp device so
+		 * src=dst. Note: in noarp, there's no mac.
+		 */
+		memcpy(pkt1->data, skb->data, 14);
+		//printk("data=%04x:%04x:%04x | %04x:%04x:%04x | %04x | %02x %02x %02x %02x ...\n",
+		//       *(u16 *)(skb->data+0),
+		//       *(u16 *)(skb->data+2),
+		//       *(u16 *)(skb->data+4),
+		//       *(u16 *)(skb->data+6),
+		//       *(u16 *)(skb->data+8),
+		//       *(u16 *)(skb->data+10),
+		//       *(u16 *)(skb->data+12),
+		//       *(u8 *)(skb->data+14),
+		//       *(u8 *)(skb->data+15),
+		//       *(u8 *)(skb->data+16),
+		//       *(u8 *)(skb->data+17));
+
+		/* build IP header */
+		*(u32 *)(pkt1->data + 14)  = htonl(0x4510002C); /* IP, tos 10, len 44 */
+		*(u16 *)(pkt1->data + 18)  = ih->id;            /* same ID as sender */
+		*(u16 *)(pkt1->data + 20)  = htons(0x4000);     /* DF, ofs=0 */
+		*(u32 *)(pkt1->data + 22)  = htonl(0x40060000); /* TTL=64, TCP, check=0 */
+		*(u32 *)(pkt1->data + 26) = ih->daddr;
+		*(u32 *)(pkt1->data + 30) = ih->saddr;
+
+		/* compute IP checksum. See also ip_send_check(). */
+		*(u16 *)(pkt1->data + 24)  = ip_fast_csum(pkt1->data + 14, 5);
+
+		/* build TCP header */
+		*(u16 *)(pkt1->data + 34) = th->dest;
+		*(u16 *)(pkt1->data + 36) = th->source;
+		*(u32 *)(pkt1->data + 38) = htonl(isn << 4);
+		*(u32 *)(pkt1->data + 42) = htonl(ntohl(th->seq) + 1);
+		*(u32 *)(pkt1->data + 46) = htonl(0x601205b4); /* doff=24, synack, win=1460 */
+		*(u32 *)(pkt1->data + 50) = 0;                 /* check, urgptr */
+		*(u32 *)(pkt1->data + 54) = htonl(0x020405b4); /* opt: MSS=<1460> */
+
+		/* compute TCP checksum */
+		*(u16*)(&pkt1->data[50]) =
+			tcp_v4_check(24,
+			             *(u32 *)(pkt1->data + 26),
+			             *(u32 *)(pkt1->data + 30),
+			             csum_partial(pkt1->data + 34, 24, 0));
+
+		/* finish the skb */
+		skb_put(pkt1, 14 + 20 + 24);
+
+		//printk("@%d: pkt1->head=%p, data=%d, tail=%d, end=%d, len=%d\n", __LINE__,
+		//       pkt1->head,
+		//       (int)(pkt1->data - pkt1->head),
+		//       (int)(skb_tail_pointer(pkt1) - pkt1->head),
+		//       (int)(skb_end_pointer(pkt1) - pkt1->head),
+		//       (int)pkt1->len);
+
+		pkt1->protocol = eth_type_trans(pkt1, dev);
+		pkt1->ip_summed = CHECKSUM_UNNECESSARY;
+		pkt1->csum = 0;
+
+		isn++;
+
+		//skb_dst_force(pkt1);
+		/* see dev_forward_skb() instead ? => no, does netif_rx() */
+		local_bh_disable();
+		netif_receive_skb(pkt1);
+		local_bh_enable();
+
+		return;
+	}
+
+	ack = th->ack_seq;
+	st = ack & 15;
+
+	return;
+}
+
+static netdev_tx_t slhttp_xmit(struct sk_buff *skb, struct net_device *dev)
+{
+	struct pcpu_dstats *dstats = this_cpu_ptr(dev->dstats);
+	int rxp, rxb;
+
+	rxp = 0; rxb = 0;
+	slhttp_reply_to_skb(dev, skb, &rxp, &rxb);
 
 	u64_stats_update_begin(&dstats->syncp);
 	dstats->tx_packets++;
-	dstats->tx_bytes += skb->len;
-	dstats->rx_packets++;
-	dstats->rx_bytes += skb->len;
+	dstats->tx_bytes   += skb->len;
+	dstats->rx_packets += rxp;
+	dstats->rx_bytes   += rxb;
 	u64_stats_update_end(&dstats->syncp);
-	return NETDEV_TX_OK;
 
- drop:
-	u64_stats_update_begin(&dstats->syncp);
-	dstats->tx_dropped++;
-	u64_stats_update_end(&dstats->syncp);
 	dev_kfree_skb(skb);
 	return NETDEV_TX_OK;
 }
